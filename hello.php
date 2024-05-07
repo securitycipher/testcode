d2d69daa2462f5ddda8d42b97576db2d05a74d8f
SecurityCipher <111536600+securitycipher@users.noreply.github.com>
2024-05-07 16:13:13 +0530

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,3 +1,20 @@
-<?php
-  echo "Hello, World!";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Input Form</title>
+</head>
+<body>
+    <h2>Enter your name:</h2>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <input type="text" name="name">
+        <input type="submit" name="submit" value="Submit">
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_POST['name'];
+        echo "<h3>Welcome, $name!</h3>";
+    }
+    ?>
+</body>
+</html>
